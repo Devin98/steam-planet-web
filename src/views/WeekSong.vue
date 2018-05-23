ac17d9189e4d1c4325477b1950c26488e2a0a52a
hzq <157547800@qq.com>
2018-05-23 14:25:48 +0800

    WeekSong

--- src/views/WeekSong.vue
+++ src/views/WeekSong.vue
@@ -1,16 +1,11 @@
 
 <template>
   <div>
-    <van-nav-bar title="每周歌单"  left-arrow class="header">
-      <van-icon name="search" slot="right" />
-    </van-nav-bar>
+    <van-nav-bar title="每周歌单"  left-arrow class="header" @click-left="clickLeft()"></van-nav-bar>
     <van-list>
-      <van-cell v-for="value in song" class="list"><img class="play" src="../assets/play.png"><p class="font16">{{value.songName}}</p><p class="font10">{{value.author}}</p></van-cell>
+      <van-cell v-for="value in song" v-on:click="clicklist()" class="list"><img class="play" src="../assets/play.png"><p class="font16">{{value.songName}}</p><p class="font10">{{value.author}}</p></van-cell>
     </van-list>
-
   </div>
-
-
 </template>
 
 <script>
@@ -18,29 +13,37 @@
   import { NavBar } from 'vant';
   import { List } from 'vant';
   import {Cell} from 'vant';
+
   Vue.use(List);
   Vue.use(Cell);
   Vue.use(NavBar);
 
-
     export default {
-        name: 'WeekSong',
-        methods: {},
+      name: 'WeekSong',
+      methods: {
+        clicklist() {
+          this.$router.push({path:'/MusicPlay'});//跳转
+        },
+        clickLeft(){
+          this.$router.push({path:'/home'});
+        }
+      },
         components: {},
         data() {
-            return {
-              song:[
-                {songName:'阴天快乐',author:'陈奕迅'},
-                {songName:'阴天快乐',author:'陈奕迅'},
-                {songName:'阴天快乐',author:'陈奕迅'},
-                {songName:'阴天快乐',author:'陈奕迅'},
-                {songName:'阴天快乐',author:'陈奕迅'},
-                {songName:'阴天快乐',author:'陈奕迅'},
-                {songName:'阴天快乐',author:'陈奕迅'}
-              ]
-            }
-        }
-    }
+          return {
+            song: [
+              {songName: '阴天快乐', author: '陈奕迅'},
+              {songName: '阴天快乐', author: '陈奕迅'},
+              {songName: '阴天快乐', author: '陈奕迅'},
+              {songName: '阴天快乐', author: '陈奕迅'},
+              {songName: '阴天快乐', author: '陈奕迅'},
+              {songName: '阴天快乐', author: '陈奕迅'},
+              {songName: '阴天快乐', author: '陈奕迅'}
+            ]
+          }
+        },
+      }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
